af45431c78151a819b23a30a814799e0a8903b3a
Darwin Rivera <riveradarwin.1991@outlook.com>
2024-06-12 04:05:17 -0500

    se estructuro calculadora y aplique algunas funciones

--- clase-05-06/JS/logica.js
+++ clase-05-06/JS/logica.js
@@ -11,6 +11,44 @@ window.open('calculadora.html')
 console.log(`Ingreso ok`)
 }else{
     console.log(`Ingreso denegado`)
-
 }
+}
+
+function n0(){
+    document.getElementById('pantalla').value+=0
+}
+
+function n1(){
+    document.getElementById('pantalla').value+=1
+}
+
+function n2(){
+    document.getElementById('pantalla').value+=2
+}
+
+function n3(){
+    document.getElementById('pantalla').value+=3
+}
+
+function n4(){
+    document.getElementById('pantalla').value+=4
+}
+function n5(){
+    document.getElementById('pantalla').value+=5
+}
+
+function n6(){
+    document.getElementById('pantalla').value+=6
+}
+
+function n7(){
+    document.getElementById('pantalla').value+=7
+}
+
+function n8(){
+    document.getElementById('pantalla').value+=8
+}
+
+function n9(){
+    document.getElementById('pantalla').value+=9
 }
