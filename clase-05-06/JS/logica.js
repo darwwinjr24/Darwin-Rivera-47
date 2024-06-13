9fd0ac3501995704c488151758c6b0a9fa51eb81
Darwin Rivera <riveradarwin.1991@outlook.com>
2024-06-13 14:18:48 -0500

    botones y operaciones se pintan en pantalla, funcion borrar ok, cambie navbar de pagina index

--- clase-05-06/JS/logica.js
+++ clase-05-06/JS/logica.js
@@ -16,16 +16,16 @@ console.log(`Ingreso ok`)
 
 function n0(){
     document.getElementById('pantalla').value+=0
-}
-
-function n1(){
-    document.getElementById('pantalla').value+=1
-}
-
-function n2(){
-    document.getElementById('pantalla').value+=2
-}
-
+    }
+    
+    function n1(){
+        document.getElementById('pantalla').value+=1
+        }
+        
+        function n2(){
+            document.getElementById('pantalla').value+=2
+            }
+            
 function n3(){
     document.getElementById('pantalla').value+=3
 }
@@ -35,20 +35,53 @@ function n4(){
 }
 function n5(){
     document.getElementById('pantalla').value+=5
-}
-
-function n6(){
+    }
+    
+    function n6(){
     document.getElementById('pantalla').value+=6
 }
 
 function n7(){
     document.getElementById('pantalla').value+=7
+    }
+    
+    function n8(){
+        document.getElementById('pantalla').value+=8
+        }
+        
+        function n9(){
+            document.getElementById('pantalla').value+=9
+            }
+
+        function suma(){
+                document.getElementById('pantalla').value+='+'               
 }
 
-function n8(){
-    document.getElementById('pantalla').value+=8
+function resta(){
+    document.getElementById('pantalla').value+='-'
 }
 
-function n9(){
-    document.getElementById('pantalla').value+=9
+function multiplicacion(){
+    document.getElementById('pantalla').value+='*'
+}
+
+function division(){
+    document.getElementById('pantalla').value+='/'
+}
+
+function borrar(){
+    document.getElementById('pantalla').value=''
+}
+
+
+sumando = []
+function igual(){
+
+   pant =  document.getElementById('pantalla').value
+   sumando.push(pant)
+   sumando.forEach(element =>{
+    document.getElementById('resultado').innerHTML=element
+
+   console.log(element)
+ })
 }
