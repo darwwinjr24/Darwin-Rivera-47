2fdbd58163cdd05b1aa3d38801a244152ec6f7f5
Darwin Rivera <riveradarwin.1991@outlook.com>
2024-07-04 18:17:00 -0500

    se creo carpeta de interfaces con su maquetacion

---
+++ clase-05-06/JS/estructura json.js
@@ -0,0 +1,23 @@
+let platosDelMenuJSON=[
+    {"nombrePlato": "Arroz Con Pollo",
+    "precioPlato": 25000,
+    "chef": "alberto",
+    "ingredientes":[
+        {"nombre":["agua",
+            "sal",
+            "pechuga",
+            "arroz",
+        ],
+        "cantidad":["2 Pocillos",
+            "al Gusto",
+            "1 Pechuga",
+            "1 pocillo",     
+    ],
+    "precio":[1000,
+        500,
+        14000,
+        2000
+    ]
+    }]
+    }
+]
